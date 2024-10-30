1a5abc8cdcebd257396439b699c4f89bae7c8722
André Freitas <dre.freitas23@gmail.com>
2024-10-30 18:32:45 -0300

    feat: ajusted tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Guys Movies</h1>;
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1 className="text-white text-4xl">Guys Movies</h1>
+    </div>
+  );
 };
 
 export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/globals.css",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        defaultBackground: "#020617",
       },
     },
   },
