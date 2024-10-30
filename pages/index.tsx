a379b30b1a00c2a18ce039f54fc952eb60a02701
André Freitas <dre.freitas23@gmail.com>
2024-10-30 19:23:58 -0300

    feat: defined font default of project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div className="flex items-center justify-center h-full">
-      <h1 className="text-white text-4xl">Guys Movies</h1>
-    </div>
-  );
+  return <h1 className="text-white text-4xl font-bold">Guys Movies</h1>;
 };
 
 export default Home;
