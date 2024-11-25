7f8d31fb8e0a19792ae12c980c4476f23460adad
AndreFreitaszz <andre.filho@kabum.com.br>
2024-11-25 09:49:10 -0300

    refactor: component `useFetch`

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Carousel from "../components/_ui/carousel";
 import LoadingSpinner from "../components/_ui/loadingSpinner";
 import { toast } from "react-toastify";
 import MoviesProvider from "../components/home/moviesProvider";
+import useFetch from "../hooks/useFetch";
 
 interface Movie {
   id: number;
@@ -26,32 +27,10 @@ interface ProviderData {
 }
 
 const Home = () => {
-  const [data, setData] = useState<ProviderData[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_URL_API}/movies/popularByProviders`,
-        );
-        if (!response.ok) {
-          throw new Error("Erro ao buscar os filmes populares");
-        }
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error: any) {
-        const errorMessage = "Ocorreu um erro ao buscar os filmes populares";
-        setError(errorMessage);
-        toast.error(errorMessage);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchData();
-  }, []);
+  const { data, isLoading, error } = useFetch<ProviderData[]>(
+    `${process.env.NEXT_PUBLIC_URL_API}/movies/popularByProviders`,
+    "Ocorreu um erro ao buscar os filmes populares",
+  );
 
   return (
     <>
@@ -60,9 +39,7 @@ const Home = () => {
         <LoadingSpinner />
       ) : error ? (
         <div className="flex justify-center mt-20 h-screen">
-          <p className="text-white font-bold text-xl">
-            Ocorreu um erro ao buscar os filmes
-          </p>
+          <p className="text-white font-bold text-xl">{error}</p>
         </div>
       ) : (
         <div className="flex flex-col px-4 sm:px-6 md:px-8 lg:px-40 w-full mt-14">
@@ -74,10 +51,13 @@ const Home = () => {
               width={32}
               height={32}
             />
-            <Title title="Filmes Populares" className="ml-2 text-left" />
+            <Title
+              title="Filmes Populares Por Streamings"
+              className="ml-2 text-left"
+            />
           </div>
           <Carousel
-            data={data}
+            data={data || []}
             renderItem={(providerData) => (
               <MoviesProvider
                 providerData={providerData}
