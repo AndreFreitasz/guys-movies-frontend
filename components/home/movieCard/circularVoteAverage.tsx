e7cce2dc6f4ad39213c2583fc61ab3d436a85bb4
André Freitas <dre.freitas23@gmail.com>
2024-12-03 23:02:25 -0300

    style: gradient colors in `progressBar`

--- components/home/movieCard/circularVoteAverage.tsx
+++ components/home/movieCard/circularVoteAverage.tsx
@@ -9,29 +9,52 @@ interface CircularVoteAverageProps {
 const CircularVoteAverage: React.FC<CircularVoteAverageProps> = ({
   vote_average,
 }) => {
+  const getGradientId = (vote: number) => {
+    if (vote >= 7) {
+      return "greenGradient";
+    } else if (vote >= 5) {
+      return "yellowGradient";
+    } else {
+      return "redGradient";
+    }
+  };
+
   const getVoteColor = (vote: number) => {
     if (vote >= 7) {
-      const green = "#65a30d";
-      return green;
+      return "#65a30d";
     } else if (vote >= 5) {
-      const yellow = "#eab308";
-      return yellow;
+      return "#eab308";
     } else {
-      const red = "#dc2626";
-      return red;
+      return "#dc2626";
     }
   };
 
   return (
     <div className={styles.circularProgressbarContainer}>
+      <svg style={{ height: 2 }}>
+        <defs>
+          <linearGradient id="greenGradient" gradientTransform="rotate(90)">
+            <stop offset="0%" stopColor="#3f6212" />
+            <stop offset="100%" stopColor="#84cc16" />
+          </linearGradient>
+          <linearGradient id="yellowGradient" gradientTransform="rotate(90)">
+            <stop offset="0%" stopColor="#854d0e" />
+            <stop offset="100%" stopColor="#fde047" />
+          </linearGradient>
+          <linearGradient id="redGradient" gradientTransform="rotate(90)">
+            <stop offset="0%" stopColor="#dc2626" />
+            <stop offset="100%" stopColor="#dc2626" />
+          </linearGradient>
+        </defs>
+      </svg>
       <CircularProgressbar
         value={vote_average * 10}
-        text={`${vote_average}`}
+        text={`${(vote_average * 10).toFixed(0)}%`}
         styles={buildStyles({
           textColor: getVoteColor(vote_average),
-          pathColor: getVoteColor(vote_average),
+          pathColor: `url(#${getGradientId(vote_average)})`,
           trailColor: "rgba(255, 255, 255, 0.2)",
-          textSize: "36px",
+          textSize: "30px",
         })}
       />
     </div>
--- components/home/movieCard/index.tsx
+++ components/home/movieCard/index.tsx
@@ -28,10 +28,12 @@ const MovieCard: React.FC<MovieCardProps> = ({
           alt={title}
           className="w-80 h-50 rounded-lg transition-opacity duration-300 group-hover:opacity-50"
         />
-        <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black via-transparent to-transparent rounded-lg p-4 w-80 h-50">
+        <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-defaultBackground via-transparent to-transparent rounded-lg p-4 w-80 h-50">
           <h3 className="text-white text-2xl font-bold mb-2">{title}</h3>
-          <p className="text-white text-sm mb-2">{overview}</p>
-          <div className="w-16 h-16">
+          <p className="text-white text-sm mb-2 line-clamp-5 font-semibold">
+            {overview}
+          </p>
+          <div className="w-16 h-16 mt-5">
             <CircularVoteAverage vote_average={vote_average} />
           </div>
         </div>
