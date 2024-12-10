a610529b5fe5d71281cf0606aa60f56fc3c50e9b
AndreFreitaszz <andre.filho@kabum.com.br>
2024-12-10 14:45:42 -0300

    feat: add footer

---
+++ components/_ui/footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="mt-24 bg-defaultBackgroundSecond p-4">
+      <p className="text-white font-medium text-sm text-center">
+        Desenvolvido por <a className="text-white">André Freitas</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
