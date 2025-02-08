2aa6d337d29fec85bec83cea58525fe078048e44
André Freitas <andre.filho@kabum.com.br>
2025-02-08 03:04:00 -0300

    feat: loading spinner

--- components/_ui/header/index.tsx
+++ components/_ui/header/index.tsx
@@ -15,6 +15,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import FormRegister from "./formRegister";
 import FormLogin from "./formLogin";
 import { useAuth } from "../../../hooks/authContext";
+import ConfirmLogoutModal from "../modal/confirmLogoutModal";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -23,20 +24,50 @@ const Header = () => {
   const [isSearchExpanded, setIsSearchExpanded] = useState(false);
   const [isSearchVisible, setIsSearchVisible] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
   const { isAuthenticated, user, loading, logout } = useAuth();
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   const handleLoginClick = () => setIsModalOpen(true);
+
   const handleRegisterClick = () => setIsRegisterModalOpen(true);
+
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   const closeModal = () => setIsModalOpen(false);
+
   const closeRegisterModal = () => setIsRegisterModalOpen(false);
+
   const handleSearchFocus = () => setIsSearchExpanded(true);
+
   const handleSearchBlur = () => setIsSearchExpanded(false);
+
   const toggleSearchBar = () => setIsSearchVisible(!isSearchVisible);
+
+  const handleLogoutClick = () => setIsLogoutModalOpen(true);
+
   const toggleDropdown = (e?: React.MouseEvent) => {
     e?.stopPropagation();
     setIsDropdownOpen((prev) => !prev);
+  };
+
+  const handleLogoutConfirm = () => {
+    setIsLogoutModalOpen(false);
+    logout();
   };
 
   return (
@@ -88,6 +119,7 @@ const Header = () => {
                   onClick={toggleDropdown}
                   icon={<FaCaretDown size={24} />}
                   className="hidden md:flex"
+                  ref={buttonRef}
                 />
                 {isDropdownOpen && (
                   <AnimatePresence>
@@ -96,7 +128,7 @@ const Header = () => {
                       initial={{ opacity: 0, y: -10 }}
                       animate={{ opacity: 1, y: 0 }}
                       transition={{ duration: 0.4 }}
-                      className="absolute right-0 mt-2 w-52 bg-defaultBackgroundSecond text-center bg-opacity-80 text-white rounded-lg shadow-xl z-20"
+                      className="absolute right-0 mt-2 w-52 text-center bg-opacity-80 text-white rounded-lg shadow-xl z-20 inline-flex items-center justify-center px-8 py-3 overflow-hidden font-bold transition-all duration-300 bg-indigo-600 ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none"
                     >
                       <ul className="divide-y divide-white/30">
                         <li className="hover:cursor-pointer">
@@ -111,7 +143,7 @@ const Header = () => {
                         </li>
                         <li>
                           <button
-                            onClick={logout}
+                            onClick={handleLogoutClick}
                             className="block w-full px-4 py-2 hover:bg-white/10 transition-colors duration-200"
                           >
                             Sair
@@ -189,6 +221,13 @@ const Header = () => {
       >
         <FormRegister onClose={closeRegisterModal} />
       </Modal>
+
+
+      <ConfirmLogoutModal
+        isOpen={isLogoutModalOpen}
+        onClose={() => setIsLogoutModalOpen(false)}
+        onConfirm={handleLogoutConfirm}
+      />
     </header>
   );
 };
